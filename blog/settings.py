be6d23ccbf99064cb307f2e7b14007f1959597e3
Roman <roman.oneshchuk@gmail.com>
2020-10-15 18:59:44 +0300

    Bloging added

--- blog/settings.py
+++ blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'events.apps.EventsConfig',
+    'bloging.apps.BlogingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- bloging/models.py
+++ bloging/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Bloging(models.Model):
+	post_image = models.ImageField(upload_to='bloging_images/')
+	post_text = models.CharField(max_length=1000)
+	post_title = models.CharField(max_length=50)
+	post_date = models.DateTimeField()
