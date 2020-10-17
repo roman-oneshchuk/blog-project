2764f3fe8c508e11f1085fef3744dd7a923b0858
Roman <roman.oneshchuk@gmail.com>
2020-10-17 14:11:36 +0300

    home page added

--- bloging/models.py
+++ bloging/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 # Create your models here.
 
 class Bloging(models.Model):
-	post_image = models.ImageField(upload_to='bloging_images/')
-	post_text = models.CharField(max_length=1000)
 	post_title = models.CharField(max_length=50)
+	post_text = models.TextField()
 	post_date = models.DateTimeField()
+	post_image = models.ImageField(upload_to='bloging_images/')
